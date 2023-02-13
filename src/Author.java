487e316575c782d9f852accb0dbb663f59257143
ch1ple <teavis@mail.ru>
2023-02-13 23:28:24 +0300

    Homework 12 complete

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String name;
+    private String surName;
+    public Author(String name, String surName) {
+        this.name = name;
+        this.surName = surName;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSurName() {
+        return this.surName;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        Author julWern = new Author("Жуль", "Верн");
+        Book captain = new Book("Пятнадцатилетний капитан", julWern, 1978);
+        // В инструкции к домашнему заданию написано, что тип полей в классе Book должен быть Author, я немного это не понял
+        System.out.println("Название книги = " + captain.getName());
+        System.out.println("Автор = " + julWern.getName() + " " + julWern.getSurName());
+        System.out.println("Год публикации = " + captain.getPublicationDate());
+        captain.setPublicationDate(1999);
+        System.out.println("Обновленный год публикации = " + captain.getPublicationDate());
+        Author arthurHailey = new Author("Артур", "Хейли");
+        Book airport = new Book("Аэропорт", new Author("Артур", "Хейли"), 1968);
+        // Попробовал разными способами выводить имя автора, но оба варианта выводит непонятные символы
+        // поэтому остановился на том, что отдельно вывожу имя автора
+        System.out.println("Название книги = " + airport.getName());
+        System.out.println("Автор = " + arthurHailey.getName() + " " + arthurHailey.getSurName());
+        System.out.println("Год публикации = " + airport.getPublicationDate());
     }
 }
